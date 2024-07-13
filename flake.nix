a92d668b21499d65b64411f7b037bdb0fecc736c
Yang, Bo <bo.yang@protonbase.io>
2024-07-13 17:44:12 +0000

    Add model_base_caption_capfilt_large.pth

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     nix-ml-ops.url = "github:Atry/nix-ml-ops";
     nix-ml-ops.inputs.systems.url = "github:nix-systems/default";
 
+    blip_models_model_base_caption_capfilt_large = {
+      url = "https://www.modelscope.cn/models/popatry/BLIP/resolve/master/models/model_base_caption_capfilt_large.pth";
+      flake = false;
+    };
     stable-diffusion_v2-1_768-ema-pruned_safetensors = {
       url = "https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.safetensors?download=true";
       flake = false;
@@ -18,7 +22,7 @@
       flake = false;
     };
   };
-  outputs = inputs :
+  outputs = inputs:
     inputs.nix-ml-ops.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.nix-ml-ops.flakeModules.devcontainer
@@ -37,20 +41,28 @@
             "${pkgs.glib.out}/lib"
             "${pkgs.libGL}/lib"
           ];
-          nixago.requests."extensions/sd-webui-stablesr/models/webui_768v_139.ckpt" = {
-            data = { };
-            engine = { data, output, ... }: inputs.StableSR_webui_768v_139_ckpt;
-          };
-          nixago.requests."models/Stable-diffusion/v1-5-pruned-emaonly.safetensors" = {
-            data = { };
-            engine = { data, output, ... }: inputs.stable-diffusion_v1-5-pruned-emaonly_safetensors;
-          };
-          nixago.requests."models/Stable-diffusion/v2-1_768-ema-pruned.safetensors" = {
-            data = { };
-            engine = { data, output, ... }: inputs.stable-diffusion_v2-1_768-ema-pruned_safetensors;
+          nixago.requests = {
+
+            "models/BLIP/model_base_caption_capfilt_large.pth" = {
+              data = { };
+              engine = { data, output, ... }: inputs.blip_models_model_base_caption_capfilt_large;
+            };
+            "extensions/sd-webui-stablesr/models/webui_768v_139.ckpt" = {
+              data = { };
+              engine = { data, output, ... }: inputs.StableSR_webui_768v_139_ckpt;
+            };
+            "models/Stable-diffusion/v1-5-pruned-emaonly.safetensors" = {
+              data = { };
+              engine = { data, output, ... }: inputs.stable-diffusion_v1-5-pruned-emaonly_safetensors;
+            };
+            "models/Stable-diffusion/v2-1_768-ema-pruned.safetensors" = {
+              data = { };
+              engine = { data, output, ... }: inputs.stable-diffusion_v2-1_768-ema-pruned_safetensors;
+            };
           };
           devenvShellModule = {
             languages = {
+              c.enable = true;
               python = {
                 enable = true;
                 venv = {
